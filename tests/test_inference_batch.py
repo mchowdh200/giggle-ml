9a6f702c2b959b0926c14169497b8303c773cfb3
Simon Walker <simon@simonwalker.tech>
2024-08-12 11:40:06 -0600

    batch inference test

---
+++ tests/test_inference_batch.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from data_wrangling.list_dataset import ListDataset
+from gpu_embeds.inference_batch import BatchInferHyenaDNA
+import numpy as np
+
+
+class IdentityModel(nn.Module):
+    def __init__(self):
+        super(IdentityModel, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class BatchInfTesting(BatchInferHyenaDNA):
+    def __init__(self):
+        embedDim = 10
+        super(BatchInfTesting, self).__init__(
+            embedDim, useDDP=False, useMeanAggregation=False)
+
+    def prepareModel(self, rank, device):
+        return IdentityModel()
+
+
+def test_inference_batch():
+    batchInf = BatchInfTesting()
+    workers = 2
+    batchSize = 3
+    count = 25
+    outPath = "./test_out.npy"
+
+    inputs = torch.zeros((count, 10), dtype=torch.float32)
+
+    for i in range(count):
+        inputs[i] = torch.tensor([i] * 10, dtype=torch.float32)
+
+    dataset = ListDataset(inputs)
+    batchInf.batchInfer(dataset, outPath, batchSize, workers)
+
+    outputs = np.memmap(outPath, dtype='float32', mode='r')
+    outputs = outputs.reshape((-1, 10))
+    inputs = inputs.numpy()
+
+    assert np.allclose(inputs, outputs)
