97f20d05ef1af0dcfe9732a1703e1db2f28578ae
Simon Walker <simon@simonwalker.tech>
2024-08-20 09:08:39 -0600

    hyenadna support working again

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,6 @@ import numpy as np
 from types import SimpleNamespace
 from data_wrangling.seq_datasets import BedDataset, FastaDataset, TokenizedDataset
 import statistical_tests.tests as tests
-from gpu_embeds.region2vec_wrapper import R2VBatchInf
 
 
 def get_intervals(bedPath):
@@ -21,13 +20,13 @@ def get_intervals(bedPath):
 
 def getInfSystem():
     # HyenaDNA
-    # return BatchInferHyenaDNA()
+    return BatchInferHyenaDNA()
 
     # Region2Vec
-    return R2VBatchInf()
+    # return R2VBatchInf()
 
 
-def embeds(limit, batchSize, paths, workers, bufferSize):
+def make_embeds(limit, batchSize, paths, workers, bufferSize):
     print("Preparing bed dataset...")
     bedDs = BedDataset(paths.bed, limit=limit,
                        inMemory=True, bufferSize=bufferSize)
@@ -36,19 +35,22 @@ def embeds(limit, batchSize, paths, workers, bufferSize):
     inf = getInfSystem()
 
     # HyenaDNA
-    # fastaDs = FastaDataset(paths.fasta, bedDs)
-    # seqDs = TokenizedDataset(fastaDs)
-    # inf.batchInfer(seqDs, paths.embeds, batchSize, workers)
+    fastaDs = FastaDataset(paths.fasta, bedDs)
+    seqDs = TokenizedDataset(fastaDs)
+    inf.batchInfer(seqDs, paths.embeds, batchSize, workers)
 
     # Region2Vec
-    inf.batchInfer(bedDs, paths.embeds, batchSize, workers)
+    # inf.batchInfer(bedDs, paths.embeds, batchSize, workers)
 
 
-def doSWT(intervals, infSystem, limit):
+def advanced_tests(intervals, embeds, infSystem, limit):
     fastaDs = FastaDataset(paths.fasta, intervals)
     tokDs = TokenizedDataset(fastaDs)
+
+    # TODO: only god knows why the reverse order causes a crash
     tests.swt(paths.embeds, fastaDs, infSystem, considerLimit=limit)
     print()
+    tests.rct(embeds, tokDs)
 
 
 def run_tests(paths, limit):
@@ -59,15 +61,14 @@ def run_tests(paths, limit):
     embedDim = infSystem.embedDim
     embeds = embeds.reshape(-1, embedDim)
 
-    # doSWT(intervals, infSystem, limit)
+    print()
+    advanced_tests(intervals, embeds, infSystem, limit)
+    print()
     tests.ctt(embeds)
     print()
     tests.gdst(embeds, intervals)
     print()
     tests.npt(embeds, intervals)
-    print()
-    # tests.rct(embeds, tokDs)
-    # print()
 
 
 if __name__ == "__main__":
@@ -76,7 +77,7 @@ if __name__ == "__main__":
     # INFO: Config
     limit = 440
     batchSize = 11
-    workers = 1
+    workers = 10
     bufferSize = 100
 
     # paths = SimpleNamespace(
@@ -88,6 +89,6 @@ if __name__ == "__main__":
         bed="./data/synthetic/universe_0.bed",
         embeds="./data/synthetic/embeds.npy")
 
-    embeds(limit, batchSize, paths, workers, bufferSize)
+    # make_embeds(limit, batchSize, paths, workers, bufferSize)
     run_tests(paths, limit)
     # genom_main(limit, batchSize, paths.embeds)
