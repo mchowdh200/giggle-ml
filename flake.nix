22f0490107c31f1381632e873312e7b8bbaab269
Simon Walker <simon@simonwalker.tech>
2025-03-30 23:26:30 -0600

    updated flake to 24.11

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   description = "Giggle ML";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ 
+          packages = with pkgs; [
             git-lfs
             htslib
             snakemake
-					];
+          ];
         };
       }
-		);
+    );
 }
