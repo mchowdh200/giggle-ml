518e938b7eea65bbcba446b8e41b5041c1917281
Simon Walker <simon@simonwalker.tech>
2025-05-08 01:15:26 -0600

    UnifiedDataset
    also: deprecated SlidingWindowDataset
    UnifiedDataset will allow reuse of a single DataLoader over multiple
    datasets by abstracting under a single interface. UnifiedDataset
    supports fast random access and incremental access so no decrease in
    performance is expected

--- tests/testDataWrangling.py
+++ tests/testDataWrangling.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 from giggleml.dataWrangling import fasta
 from giggleml.dataWrangling.intervalDataset import BedDataset
+from giggleml.dataWrangling.listDataset import ListDataset
 from giggleml.dataWrangling.slidingWindowDataset import SlidingWindowDataset
+from giggleml.dataWrangling.unifiedDataset import UnifiedDataset
 
 
 def testBedFastaParsing():
@@ -19,23 +23,44 @@ def testBedFastaParsing():
     assert fa[-2] == "AAAAA"
 
 
-def testSlidingWindowDataset():
-    # TODO: SlidingWindowDataset requires further tests.
-    # there's weird stuff going on
+# def testSlidingWindowDataset():
+#     # TODO: SlidingWindowDataset requires further tests.
+#     # there's weird stuff going on
+#
+#     content = [
+#         "Steaks are best hot!",
+#         "123456789",
+#     ]
+#     dataset = SlidingWindowDataset(content, 0.5)
+#     expecting = [
+#         "Steaks are",
+#         "s are best",
+#         " best hot!",
+#         "1234",
+#         "3456",
+#         "5678",
+#     ]
+#
+#     for i, expect in enumerate(expecting):
+#         assert expect == dataset[i]
 
-    content = [
-        "Steaks are best hot!",
-        "123456789",
-    ]
-    dataset = SlidingWindowDataset(content, 0.5)
-    expecting = [
-        "Steaks are",
-        "s are best",
-        " best hot!",
-        "1234",
-        "3456",
-        "5678",
-    ]
 
-    for i, expect in enumerate(expecting):
-        assert expect == dataset[i]
+def testUnifiedDataset():
+    sizes = [5, 20, 8, 13]
+    lists = [list(range(i)) for i in sizes]
+    datasets = [ListDataset(items) for items in lists]
+    uniSet = UnifiedDataset[int](datasets)
+    expect = np.concatenate(lists)
+
+    assert len(uniSet) == len(expect)
+
+    for i, item in enumerate(expect):
+        assert item == uniSet[i]
+
+    np.random.seed(42)
+    walk = list(range(len(expect)))
+    np.random.shuffle(walk)
+
+    for i in walk:
+        # tests random access
+        assert expect[i] == uniSet[i]
