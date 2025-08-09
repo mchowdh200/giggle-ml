42645ef5188346e0ae6d3b263c8a144a5df00d5f
Simon Walker <simon@simonwalker.tech>
2025-08-09 02:45:23 -0600

    path_utils, list split utils

--- src/giggleml/utils/misc.py
+++ src/giggleml/utils/misc.py
@@ -1,35 +1,41 @@
-import os
-from pathlib import Path
+from typing import cast
+
+from torch import Tensor
 
 
-def fix_bed_ext[T: str | Path](path: T) -> T:
+def partition_integer(n: int, k: int) -> list[int]:
     """
-    Fixes the extension to either be .bed or .bed.gz
-    depending on what exists. Tries adding/removing .gz
-    or adding .bed to make it exist.
+    Splits an integer n into k parts as evenly as possible.
 
-    @returns same type in
+    Args:
+      n: The integer to split.
+      k: The number of parts.
 
-    Throws if no permutation exists.
+    Returns:
+      A list of k integers that sum to n.
     """
+    if k <= 0:
+        return []
 
-    wants_str = isinstance(path, str)
+    base_size = n // k
+    remainder = n % k
 
-    if not wants_str:
-        path = str(path)
+    parts = []
+    for i in range(k):
+        if i < remainder:
+            parts.append(base_size + 1)
+        else:
+            parts.append(base_size)
 
-    if not path.endswith(".bed.gz") and not path.endswith(".bed"):
-        path += ".bed.gz"
+    return parts
 
-    if not os.path.isfile(path):
-        if path.endswith(".bed.gz"):
-            path = path[:-3]
-        elif path.endswith(".bed"):
-            path += ".gz"
 
-    if os.path.isfile(path):
-        if wants_str:
-            return path
-        return Path(path)
-
-    raise FileNotFoundError(path)
+def partition_list[T: list | Tensor](items: T, world_size: int, rank: int) -> T:
+    """
+    Splits a list into partitions, as evenly possible, and returns the
+    parition corresponding to the rank.
+    """
+    splits = partition_integer(len(items), world_size)
+    i = sum(splits[:rank])
+    part = splits[rank]
+    return cast(T, items[i : i + part])
