c3d771a92c7741d1f9793abef5307d55fda3ff01
Simon Walker <simon@simonwalker.tech>
2025-09-24 21:15:56 -0600

    MModel: plan

--- src/giggleml/embed_gen/embed_model/m_model.py
+++ src/giggleml/embed_gen/embed_model/m_model.py
@@ -1,1 +1,19 @@
-# M Model
+"""
+M Model, deep sets architecture, hyenaDNA pre-processing
+
+    (interval set -> sequence set --)-> sequence embeddings -> [M Model] -> (a single) set embedding
+
+            | hyenaDNA  | M Model Core
+            |           |   MLP, "phi"
+            |           |     (!) activations NOT saved by default
+            |           |
+    ACGT... -> A-vec   --> B-vec    ---
+    ACGT... -> A-vec   --> B-vec     | mean          MLP, "row"
+    ACGT... -> A-vec   --> B-vec     |------> B-vec ------------> C-vec
+    ACGT... -> A-vec   --> B-vec     |
+    ACGT... -> A-vec   --> B-vec    ---
+                   |
+                   |
+   could be memoized
+
+"""
