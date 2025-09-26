2543f9d5b7ce6742236902759c66aa912d7e618e
Simon Walker <simon@simonwalker.tech>
2025-09-26 11:37:30 -0600

    torch utils: freeze_model

--- src/giggleml/utils/torch_utils.py
+++ src/giggleml/utils/torch_utils.py
@@ -8,4 +8,14 @@ def guess_device(rank: int = 0) -> Device:
             return torch.device(f"mps:{rank}")  # for mac use
         if torch.cuda.is_available():
             return torch.device(f"cuda:{rank}")
+
     return torch.device("cpu")
+
+
+def freeze_model[T: torch.nn.Module](model: T, unfreeze: bool = False) -> T:
+    """modifies the model in place"""
+
+    for param in model.parameters():
+        param.requires_grad = unfreeze
+
+    return model
