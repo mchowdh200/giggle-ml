96d0bbd48ec39219879f2a16398622272757a18b
Simon Walker <simon@simonwalker.tech>
2025-10-09 03:24:06 -0600

    nothing

---
+++ src/giggleml/utils/nothing.py
@@ -0,0 +1,21 @@
+from typing import overload
+
+
+# it does nothing
+class Nothing:
+    """A callable that does nothing but return its first argument unchanged or None."""
+
+    @overload
+    def __call__(self) -> None: ...
+
+    @overload
+    def __call__[T](self, arg: T) -> T: ...
+
+    @overload
+    def __call__[T](self, arg: T, *args: object) -> T: ...
+
+    def __call__[T](self, arg: T | None = None, *args: object) -> T | None:
+        return arg
+
+
+nothing = Nothing()
