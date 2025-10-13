b9032cfbc9042c0ccf9b95128c08c8a7701d3cd8
Simon Walker <simon@simonwalker.tech>
2025-10-13 16:19:42 -0600

    iter_utils: fix __all__

--- src/giggleml/iter_utils/__init__.py
+++ src/giggleml/iter_utils/__init__.py
@@ -1,6 +1,6 @@
 from .rank_iter import RankIter
-from .rank_scatter_mean import rank_scatter_mean, rank_scatter_mean_iter
+from .distributed_scatter_mean import distributed_scatter_mean, distributed_scatter_mean_iter
 from .set_flat_iter import SetFlatIter
 
-__all__ = ["RankIter", "SetFlatIter", "rank_scatter_mean", "rank_scatter_mean_iter"]
+__all__ = ["RankIter", "SetFlatIter", "distributed_scatter_mean", "distributed_scatter_mean_iter"]
 
