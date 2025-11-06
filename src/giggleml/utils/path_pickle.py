478c1b43b7aca5bbd922a2c3ea49863bf88fbece
Simon Walker <simon@simonwalker.tech>
2025-11-06 04:01:45 -0700

    path pickle util, PathLike

--- src/giggleml/utils/path_pickle.py
+++ src/giggleml/utils/path_pickle.py
@@ -1,12 +1,14 @@
 import pickle as _pickle
 from typing import Any
 
+from giggleml.utils.types import PathLike
 
-def pickle(path: str, data: Any):
+
+def pickle(path: PathLike, data: Any):
     with open(path, "wb") as file:
         _pickle.dump(data, file)
 
 
-def unpickle(path: str):
+def unpickle(path: PathLike):
     with open(path, "rb") as file:
         return _pickle.load(file)
